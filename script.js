7735b1ba235ff70636bdce622d69bce196997c3c
DevSeldron <sudohct@viiah.com>
2023-05-23 20:15:27 +0200

    popup redir fix

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
-// script.js
+window.addEventListener('DOMContentLoaded', function() {
+  // Function to show the popup and handle the redirection
+  function showPopup(event, url) {
+    event.preventDefault(); // Prevent the default link behavior
+    const popup = document.getElementById('popup');
+    popup.textContent = 'Redirecting to ' + url + '...';
+    popup.classList.add('show');
+    setTimeout(function() {
+      window.location.href = url; // Redirect to the clicked URL
+    }, 5000);
+  }
 
-// Function to show the popup and handle the redirection
-function showPopup(event, url) {
-  event.preventDefault(); // Prevent the default link behavior
-  var popup = document.getElementById("popup");
-  popup.classList.add("show");
-  setTimeout(function() {
-    window.location.href = url; // Redirect to the clicked URL
-  }, 5000);
-}
-
-// Add event listeners to the links
-var links = document.querySelectorAll(".menu a");
-for (var i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", function(event) {
-    var url = this.getAttribute("href");
-    showPopup(event, url);
+  // Add event listener to the document
+  document.addEventListener('click', function(event) {
+    // Check if the clicked element is an anchor tag
+    if (event.target.tagName === 'A') {
+      const url = event.target.getAttribute('href');
+      showPopup(event, url);
+    }
   });
-}
-
-var buttons = document.querySelectorAll(".button");
-for (var i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", function(event) {
-    var url = this.getAttribute("href");
-    showPopup(event, url);
-  });
-}
+});
