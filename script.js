63fed2c97a753c1c4287b624d4ae24e152466d7c
DevSeldron <sudohct@viiah.com>
2023-05-23 16:51:51 +0200

    fixed cookie issue
    fixed cookie issue where your preferred set theme doesnt persist on other pages.

--- script.js
+++ script.js
@@ -35,3 +35,17 @@ function activateSecretMode() {
     toggleButton.classList.remove('disabled'); // Remove the disabled class
   }, 3000);
 }
+// Check if the theme preference is already set in local storage
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+  body.classList.add(savedTheme);
+}
+
+toggleButton.addEventListener('click', function() {
+  body.classList.toggle('dark-mode');
+  body.classList.toggle('light-mode');
+
+  // Store the current theme preference in local storage
+  const currentTheme = body.classList.contains('dark-mode') ? 'dark-mode' : 'light-mode';
+  localStorage.setItem('theme', currentTheme);
+});
