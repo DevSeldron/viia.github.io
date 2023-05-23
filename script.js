4f3b2ab8448f91769b40b4b53ba07a9c505d534c
DevSeldron <sudohct@viiah.com>
2023-05-23 20:10:31 +0200

    awesome popup

--- script.js
+++ script.js
@@ -1,60 +1,28 @@
-window.addEventListener('DOMContentLoaded', function() {
-  const themeToggle = document.getElementById('change-theme-button');
-  const body = document.body;
+// script.js
 
-  themeToggle.addEventListener('click', function() {
-    body.classList.toggle('dark-theme');
-    body.classList.toggle('light-theme');
-  });
-});
-
-  // Toggle theme on button click
-  themeToggleButton.addEventListener("click", function() {
-    if (container.classList.contains("dark-theme")) {
-      container.classList.remove("dark-theme");
-      container.classList.add("light-theme");
-      localStorage.setItem("theme", "light-theme");
-    } else {
-      container.classList.remove("light-theme");
-      container.classList.add("dark-theme");
-      localStorage.setItem("theme", "dark-theme");
-    }
-  });
-window.addEventListener('DOMContentLoaded', function() {
-  const themeToggle = document.getElementById('change-theme-button');
-  const body = document.body;
-
-  themeToggle.addEventListener('click', function() {
-    body.classList.toggle('dark-theme');
-    body.classList.toggle('light-theme');
-  });
-// Select the button
-const btn = document.querySelector(".change-theme-button");
-// Select the theme preference from localStorage
-const currentTheme = localStorage.getItem("theme");
-// If the current theme in localStorage is "dark"...
-
-if (currentTheme == "dark") {
-  // ...then use the .dark-theme class
-  document.body.classList.add("dark-theme");
+// Function to show the popup and handle the redirection
+function showPopup(event, url) {
+  event.preventDefault(); // Prevent the default link behavior
+  var popup = document.getElementById("popup");
+  popup.classList.add("show");
+  setTimeout(function() {
+    window.location.href = url; // Redirect to the clicked URL
+  }, 5000);
 }
-// Listen for a click on the button 
-  
-btn.addEventListener("click", function() {
-  // Toggle the .dark-theme class on each click
-  document.body.classList.toggle("dark-theme");
-  
-  // Let's say the theme is equal to light
-  
-  let theme = "light";
-  // If the body contains the .dark-theme class...
-  if (document.body.classList.contains("dark-theme")) {
-    // ...then let's make the theme dark
-    
-    theme = "dark";
-  }
-  // Then save the choice in localStorage
-  
-  localStorage.setItem("theme", theme);
-});
 
+// Add event listeners to the links
+var links = document.querySelectorAll(".menu a");
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function(event) {
+    var url = this.getAttribute("href");
+    showPopup(event, url);
+  });
+}
+
+var buttons = document.querySelectorAll(".button");
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function(event) {
+    var url = this.getAttribute("href");
+    showPopup(event, url);
+  });
+}
