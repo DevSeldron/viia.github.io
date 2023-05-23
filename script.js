fe0cbf43570606266e0d86ddc186d3acc61d2370
DevSeldron <sudohct@viiah.com>
2023-05-23 15:17:50 +0200

    more cookie integration
    woohoo

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ const toggleButton = document.getElementById('mode-toggle');
 const body = document.body;
 const container = document.getElementById('container');
 
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+  body.classList.add(savedTheme);
+}
+
 toggleButton.addEventListener('click', function() {
   body.classList.toggle('dark-mode');
   body.classList.toggle('light-mode');
+
+  // Store the current theme preference in local storage
+  const currentTheme = body.classList.contains('dark-mode') ? 'dark-mode' : 'light-mode';
+  localStorage.setItem('theme', currentTheme);
 });
