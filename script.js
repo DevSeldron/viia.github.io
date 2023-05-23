101a14277fb02de98b640e3bf43bd16e01e35552
DevSeldron <sudohct@viiah.com>
2023-05-23 16:43:43 +0200

    easter egg related to changing themes
    an easter was added related to the change theme button..!

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 const toggleButton = document.getElementById('mode-toggle');
 const body = document.body;
 
-// Check if the theme preference is already set in local storage
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme) {
-  body.classList.add(savedTheme);
-}
+let clickCounter = 0;
+let secretModeTimeout;
 
-toggleButton.addEventListener('click', function() {
-  body.classList.toggle('dark-mode');
-  body.classList.toggle('light-mode');
+toggleButton.addEventListener('click', function(event) {
+  event.preventDefault(); // Prevent the default link behavior
+
+  clearTimeout(secretModeTimeout); // Clear the previous timeout
+
+  clickCounter++;
+
+  // Check if the secret theme should be triggered
+  if (clickCounter >= 5) {
+    clickCounter = 0; // Reset the click counter
+    activateSecretMode();
+  } else {
+    body.classList.toggle('dark-mode');
+    body.classList.toggle('light-mode');
+  }
 
   // Store the current theme preference in local storage
   const currentTheme = body.classList.contains('dark-mode') ? 'dark-mode' : 'light-mode';
   localStorage.setItem('theme', currentTheme);
 });
+
+function activateSecretMode() {
+  body.classList.add('secret-mode');
+  toggleButton.classList.add('disabled'); // Add a disabled class to style the disabled button appearance
+
+  // Clear the secret mode after 3 seconds
+  secretModeTimeout = setTimeout(function() {
+    body.classList.remove('secret-mode');
+    toggleButton.classList.remove('disabled'); // Remove the disabled class
+  }, 3000);
+}
