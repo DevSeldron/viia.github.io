5d1ab8f4188e5813c67af2e961a6b88fa4021191
DevSeldron <sudohct@viiah.com>
2023-05-23 19:03:03 +0200

    my mental health
    is getting worse by the second i commit new changes to this repository.

--- script.js
+++ script.js
@@ -8,13 +8,6 @@ window.addEventListener('DOMContentLoaded', function() {
   });
 });
 
-
-  // Check if theme preference is stored in local storage
-  const currentTheme = localStorage.getItem("theme");
-  if (currentTheme) {
-    container.classList.add(currentTheme);
-  }
-
   // Toggle theme on button click
   themeToggleButton.addEventListener("click", function() {
     if (container.classList.contains("dark-theme")) {
@@ -35,5 +28,33 @@ window.addEventListener('DOMContentLoaded', function() {
     body.classList.toggle('dark-theme');
     body.classList.toggle('light-theme');
   });
+// Select the button
+const btn = document.querySelector(".change-theme-button");
+// Select the theme preference from localStorage
+const currentTheme = localStorage.getItem("theme");
+// If the current theme in localStorage is "dark"...
+
+if (currentTheme == "dark") {
+  // ...then use the .dark-theme class
+  document.body.classList.add("dark-theme");
+}
+// Listen for a click on the button 
+  
+btn.addEventListener("click", function() {
+  // Toggle the .dark-theme class on each click
+  document.body.classList.toggle("dark-theme");
+  
+  // Let's say the theme is equal to light
+  
+  let theme = "light";
+  // If the body contains the .dark-theme class...
+  if (document.body.classList.contains("dark-theme")) {
+    // ...then let's make the theme dark
+    
+    theme = "dark";
+  }
+  // Then save the choice in localStorage
+  
+  localStorage.setItem("theme", theme);
 });
 
